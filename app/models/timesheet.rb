79254434722754426da20c637fee8b7a5a01b1de
justina.maleckaite <justina.maleckaite@montedelgallo.com>
2021-01-21 11:59:29 +0100

    Updated public activity on time entries

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,6 +1,11 @@
 class Timesheet < ApplicationRecord
    belongs_to :user
    belongs_to :project, optional: true
+
+   accepts_nested_attributes_for :project
+
+   include PublicActivity::Model 
+   tracked owner: Proc.new { |controller, model| controller.current_user }
   
     
 end
